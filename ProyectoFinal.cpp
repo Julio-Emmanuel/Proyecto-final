1a9331e97ffff4fe20a0dd6d5b18b8793c806192
Julio-Emmanuel <julio.emmanuel.0504@outlook.com>
2023-11-20 10:03:05 -0600

    cambio de while a do while para mejorar

--- ProyectoFinal.cpp
+++ ProyectoFinal.cpp
@@ -5,49 +5,65 @@ int main() {
     int opcion;
     int dinero = 0; //variable para el dinero del usuario
     char eleccion;
-    while(true){
-        cout << "estamos en fes aragón estas a una semana de terminar el semestre y tienes muchos trabajos, examenes y peroyectos pesados." << endl<< "estas llegando a tu escuela y ves que se le cae el monedero a una persona delante tuyo."<< endl<< "¿qué quieres hacer?"<<endl;
-        cout << "\n--- Economía ---" << endl;
-        cout << "Dinero: " << dinero << endl; 
-        cout << "\n--- Menú de Acciones ---" << endl;
-        cout << "1. robar el monedero" << endl;
-        cout << "2. avisarle" << endl;
-        cout << "3. seguir como si nada" << endl;
-        cout << "4. pensarlo" << endl;
-        cout << "5. Presion si quieres dejar de jugar" << endl;
-        cout << "\nElige una opción: ";
-        cin>>opcion;
-        if (opcion < 1 || opcion > 5){
-            cout<<"ingresa un numero del 1 al 5"<<endl;
-        }
+
+    cout<<"bienvenido a mi juego de decisiones en c++"<<endl;
+    cout << "\n--- Menú Principal ---" << endl;
+    cout << "1. iniciar a jugar" << endl;
+    cout << "2. dinero infinito" << endl;
+    cout << "3. easter egg" << endl;
+    cout << "4. ir al final" << endl;
+    cout << "5. cerrar el juego" << endl;
+    cout << "\nElige una opción: ";
+    cin>>opcion;
+    switch(opcion){
+        case 1:
+
+    
+
+        do{
+            cout << "estamos en fes aragón estas a una semana de terminar el semestre y tienes muchos trabajos, examenes y peroyectos pesados." << endl<< "estas llegando a tu escuela y ves que se le cae el monedero a una persona delante tuyo."<< endl<< "¿qué quieres hacer?"<<endl;
+            cout << "\n--- Economía ---" << endl;
+            cout << "Dinero: " << dinero << endl; 
+            cout << "\n--- Menú de Interaccíon ---" << endl;
+            cout << "1. robar el monedero" << endl;
+            cout << "2. avisarle" << endl;
+            cout << "3. seguir como si nada" << endl;
+            cout << "\nElige una opción: ";
+            cin>>opcion;
+            if (opcion < 1 || opcion > 3){
+                cout<<"ingresa un numero del 1 al 3"<<endl;
+            }
+        }while(opcion<1||opcion>3);
         switch(opcion){
             case 1:
             cout<<"felicidades adrentro habia $50 ahora ya tienes dienero"<<endl;
             dinero += 50;
             cout<<"fueron añadidos 50 pesos a tu economia"<<endl;
-           
-            while(true){
+            break;    
+        
+            
+            
+
+                
+
+        }
+        do{
                 cout<< "acabas de entrar a tu clase de algebra y la profesora les dice que tiene hambre, así que les da la clase, ¿a donde queires ir ahora?";
                 cout << "\n--- Economía ---" << endl;
                 cout << "Dinero: " << dinero << endl; 
-                cout << "\n--- Menú de Acciones ---" << endl;
+                cout << "\n--- Menú de Interacción ---" << endl;
                 cout << "1. ir con tus amigos al billar" << endl;
                 cout << "2. ir a comer algo" << endl;
                 cout << "3. esperar en el salon a la siguiente clase " << endl;
-                cout << "4. pensarlo" << endl;
-                cout << "5. Presion si quieres dejar de jugar" << endl;
                 cout << "\nElige una opción: ";
                 cin>>opcion;
-                if (opcion < 1 || opcion > 5){
-                    cout<<"ingresa un numero del 1 al 5"<<endl;
+                if (opcion < 1 || opcion > 3){
+                    cout<<"ingresa un numero del 1 al 3"<<endl;
                 }
-            }
-
-            
-
-        }
-    }
-   
+            }while(opcion<1||opcion>3);
+        
+        
+    }   
     return 0;
 }
     
