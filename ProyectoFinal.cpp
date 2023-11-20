add1e383e0b11f22f819139933518ab32b7ff8cc
Julio-Emmanuel <julio.emmanuel.0504@outlook.com>
2023-11-20 10:05:19 -0600

    primeras 3 deciciones con caso 1 hechas

--- ProyectoFinal.cpp
+++ ProyectoFinal.cpp
@@ -27,7 +27,7 @@ int main() {
 
     
 
-        do{
+        do{//primera decision
             cout << "estamos en fes aragón estas a una semana de terminar el semestre y tienes muchos trabajos, examenes y peroyectos pesados." << endl<< "estas llegando a tu escuela y ves que se le cae el monedero a una persona delante tuyo."<< endl<< "¿qué quieres hacer?"<<endl;
             cout << "\n--- Economía ---" << endl;
             cout << "Dinero: " << dinero << endl; 
@@ -47,6 +47,8 @@ int main() {
             dinero += 50;
             cout<<"fueron añadidos 50 pesos a tu economia"<<endl;
             break;
+            case 2:
+            
         }         
 
         
@@ -56,20 +58,51 @@ int main() {
                 
 
         
-        do{
-                cout<< "acabas de entrar a tu clase de algebra y la profesora les dice que tiene hambre, así que les da la clase, ¿a donde queires ir ahora?";
-                cout << "\n--- Economía ---" << endl;
-                cout << "Dinero: " << dinero << endl; 
-                cout << "\n--- Menú de Interacción ---" << endl;
-                cout << "1. ir con tus amigos al billar" << endl;
-                cout << "2. ir a comer algo" << endl;
-                cout << "3. esperar en el salon a la siguiente clase " << endl;
-                cout << "\nElige una opción: ";
-                cin>>opcion;
-                if (opcion < 1 || opcion > 3){
-                    cout<<"ingresa un numero del 1 al 3"<<endl;
+        do{//desicion 2
+            cout<< "acabas de entrar a tu clase de algebra y la profesora les dice que tiene hambre, así que les da la clase, ¿a donde queires ir ahora?";
+            cout << "\n--- Economía ---" << endl;
+            cout << "Dinero: " << dinero << endl; 
+            cout << "\n--- Menú de Interacción ---" << endl;
+            cout << "1. ir con tus amigos al billar -40 pesos" << endl;
+            cout << "2. ir a comer algo -50 pesos" << endl;
+            cout << "3.  " << endl;
+            cout << "\nElige una opción: ";
+            cin>>opcion;
+            if (opcion < 1 || opcion > 3){
+                cout<<"ingresa un numero del 1 al 3"<<endl;
+            }
+            if(opcion == 1)
+            {
+                if (dinero <40)
+                {
+                    cout<<"no tines dinero para ir al billar"<<endl;
+                    opcion=9;
                 }
+                    
+            }
+
+        }while(opcion<1||opcion>3);    
+        switch (opcion)
+        {
+        case 1:
+            cout<<"fuiste con tus amigos al billar y te divertiste mucho definitivamente esto es algo que extrañaras del semestre, ojalá la vida fuera así siempre"<<endl;
+            dinero -= 40;
+            break;
+            
+        }
+        do{//desicion 3
+            cout<<"tu amigo Emmanuel te pide ayuda para estudiar para uno de sus examenes"<<endl;
+            cout << "\n--- Economía ---" << endl;
+            cout << "Dinero: " << dinero << endl; 
+            cout << "\n--- Menú de Interacción ---" << endl;
+            cout << "1. ayudarlo" << endl;
+            cout << "2. " << endl;
+            cout << "3. ignorarlo " << endl;
+            cout << "\nElige una opción: ";
         }while(opcion<1||opcion>3);
+
+        
+
        
         
             
